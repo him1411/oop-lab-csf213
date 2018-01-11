1bc166bea9684404a1425eccb4eb2af6e0b4dcd0
Himanshu Gupta <himjan1416@gmail.com>
2018-01-11 17:27:28 +0530

    1st lab work pushed

---
+++ lab1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class oops1
+{
+	public static void main (String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		double x = 2.0,y=3.0;
+		System.out.println("enter number " );
+		int num = scan.nextInt();
+		System.out.println("The number entered by user: "+num);
+	}
+}
