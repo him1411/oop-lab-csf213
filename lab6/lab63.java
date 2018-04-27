21995e3f6fa134ba05db955565dabe5b3ef570af
Himanshu Gupta <him141195@gmail.com>
2018-04-27 18:06:15 +0530

    practising input operations

---
+++ lab6/lab63.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class lab63
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			System.out.println("Enter a value");
+			DataInputStream dis = new DataInputStream(System.in);
+			int val = Integer.parseInt(dis.readLine());
+			
+		}	
+	}
+}
